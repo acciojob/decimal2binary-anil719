f0779cc5eb4ad9f1c67f36e1fe04a1d9e509d0f7
Anil Terapalli <117853771+anil719@users.noreply.github.com>
2023-10-12 21:12:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function decimalToBinary(num) {
 	  let answer = "";
 	  while(num != 0){
 		  answer = (num % 2) + answer ;
-		  num = num/2;
+		  num =  Math.floor(num / 2);
 	  }
 	return answer;
 }
